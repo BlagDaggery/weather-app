cb96f00c3c3da6940e8b8d0e3cf76ad15e9e4e73
Blaine Anderson <andersonblaine90@gmail.com>
2018-05-20 14:03:34 -0400

    Successfully present API data in index.html

--- js/main.js
+++ js/main.js
@@ -4,12 +4,20 @@ let latitude;
 
 $(document).ready(function() {
   navigator.geolocation.getCurrentPosition(function(position) {
-    longitude = Math.round(position.coords.longitude);
-    latitude = Math.round(position.coords.latitude);
+    longitude = position.coords.longitude;
+    latitude = position.coords.latitude;
     let endpoint = `${api}/api/current?lat=${latitude}&lon=${longitude}`;
     $.get(endpoint, function(data) {
       console.log(data);
       console.log("Success!");
+      $("#location").html(`${data.name}, ${data.sys.country}`);
+      $("#temp").html(data.main.temp);
+      $("#low").html(data.main.temp_min);
+      $("#high").html(data.main.temp_max);
+      $("#category").html(data.weather[0].main);
+      $("#description").html(data.weather[0].description);
+      let iconURL = data.weather[0].icon;
+      $("#icon").html(`<img src=${iconURL} />`);
     });
   });
 });
