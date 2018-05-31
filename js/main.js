f344e8872b5ddbf824838fd083416bb3f9895726
Blaine Anderson <andersonblaine90@gmail.com>
2018-05-30 20:43:46 -0400

    Create function to toggle between Celsius and Fahrenheit

--- js/main.js
+++ js/main.js
@@ -1,23 +1,45 @@
-let api = "https://fcc-weather-api.glitch.me/";
-let longitude;
-let latitude;
+let api = "https://fcc-weather-api.glitch.me/api/current";
+
+function convertToFahrenheit(temperature) {
+  return temperature * 9/5 + 32;
+}
 
 $(document).ready(function() {
   navigator.geolocation.getCurrentPosition(function(position) {
-    longitude = position.coords.longitude;
-    latitude = position.coords.latitude;
-    let endpoint = `${api}/api/current?lat=${latitude}&lon=${longitude}`;
+    let longitude = position.coords.longitude;
+    let latitude = position.coords.latitude;
+    let endpoint = `${api}?lat=${latitude}&lon=${longitude}`;
+
     $.get(endpoint, function(data) {
-      console.log(data);
-      console.log("Success!");
+      let iconURL = data.weather[0].icon;
+      let temperatureCelsius = Math.round(data.main.temp);
+      let temperatureFahrenheit = Math.round(convertToFahrenheit(temperatureCelsius));
+      let lowCelsius = data.main.temp_min;
+      let lowFahrenheit = Math.round(convertToFahrenheit(lowCelsius));
+      let highCelsius = data.main.temp_max;
+      let highFahrenheit = Math.round(convertToFahrenheit(highCelsius));
+      let temperatureUnit = " &deg;F";
+
       $("#location").html(`${data.name}, ${data.sys.country}`);
-      $("#temp").html(data.main.temp);
-      $("#low").html(data.main.temp_min);
-      $("#high").html(data.main.temp_max);
+      $("#icon").html(`<img src=${iconURL} />`);
       $("#category").html(data.weather[0].main);
       $("#description").html(data.weather[0].description);
-      let iconURL = data.weather[0].icon;
-      $("#icon").html(`<img src=${iconURL} />`);
+      $("#temp").html(temperatureFahrenheit + temperatureUnit);
+      $("#low").html(lowFahrenheit + temperatureUnit);
+      $("#high").html(highFahrenheit + temperatureUnit);
+      $("#temp-switch").click(function() {
+        if(temperatureUnit === " &deg;F") {
+          temperatureUnit = " &deg;C";
+          $("#temp").html(temperatureCelsius + temperatureUnit);
+          $("#low").html(lowCelsius + temperatureUnit);
+          $("#high").html(highCelsius + temperatureUnit);
+        } else if (temperatureUnit === " &deg;C") {
+          temperatureUnit = " &deg;F";
+          $("#temp").html(temperatureFahrenheit + temperatureUnit);
+          $("#low").html(lowFahrenheit + temperatureUnit);
+          $("#high").html(highFahrenheit + temperatureUnit);
+        }
+      });
     });
   });
 });
