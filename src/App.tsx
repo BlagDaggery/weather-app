a7254b1d25ce30274a9f5283a494486b165726ee
Blaine Anderson <andersonblaine90@gmail.com>
2023-10-02 11:57:14 -0400

    begin resetting template

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,39 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0);
+
+  let api: string = 'https://fcc-weather-api.glitch.me/api/current';
+
+  const [city, setCity] = useState('Your City');
+
+  function getWeatherData() {
+    navigator.geolocation.getCurrentPosition(success, error);
+  }
+
+  function success(positionData) {
+    let longitude = positionData.coords.longitude;
+    let latitude = positionData.coords.latitude;
+    let endpoint: string = `${api}?lat=${latitude}&lon=${longitude}`;
+
+    // do fetch/async + await here
+
+  }
+
+  function error(errorResponse) {
+    console.error(errorResponse);
+    // do something on the front end to let the user know it didn't work
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>How's the Weather in {city}?</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
